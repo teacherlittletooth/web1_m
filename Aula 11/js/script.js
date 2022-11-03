32bdf12cbcc5f361398500e0d7a7874e2df52351
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2022-11-03 12:14:41 -0300

    Aula 11

---
+++ Aula 11/js/script.js
@@ -0,0 +1,14 @@
+// alert("Hello world!"); //Cria uma caixa de mensagem
+// confirm("Está gostando da aula?"); //Caixa de pergunta com opção cancelar (bool)
+// prompt("Qual seu nome?"); //Caixa de pergunta com opção de digitar resposta
+
+function pegaEmail() {
+    var email = document.getElementById("floatingInput").value;
+    console.log(email);
+}
+
+function pegaPassword() {
+    var password = document.getElementById("floatingPassword").value;
+    console.log(password);
+}
+

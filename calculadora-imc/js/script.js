518c3b40bb9855b733ad923eee674a884e927fe9
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2022-11-22 21:41:01 -0300

    Update script.js

--- calculadora-imc/js/script.js
+++ calculadora-imc/js/script.js
@@ -1,27 +1,32 @@
 var idButton = document.getElementById("id-button");
 
-idButton.addEventListener("click", function(){
+idButton.addEventListener("click",function(){
     var idName = document.getElementById("id-name").value;
-    var idWeight = Number(document.getElementById("id-weight").value);
     var idHeight = Number(document.getElementById("id-height").value);
-    //console.log(typeof(idWeight));
+    var idWeight = Number(document.getElementById("id-weight").value);
     
-    var imc = idWeight / Math.pow(idHeight, 2);
-    console.log(imc.toFixed(3));
+    //console.log(typeof(idHeight));
 
-    if(imc < 18.5){
-        console.log("abaixo do peso");
-    }else if(imc >= 18.5 && imc < 25){
-        console.log("peso normal");
-    }else if(imc >= 25 && imc < 30){
-        console.log("acima do peso");
-    }else if(imc >= 30 && imc < 40){
-        console.log("obeso");
-    }else{
-        console.log("obesidade mórbida");
+    var imc = idWeight / Math.pow(idHeight, 2);
+
+    console.log("Seu IMC: " +  imc.toFixed(3));
+
+    if(imc < 18.5) {
+        console.log("Abaixo do peso");
+    } else if(imc >= 18.5 && imc < 25) {
+        console.log("Peso normal");
+    } else if(imc >= 25 && imc < 30) {
+        console.log("Acima do peso");
+    } else if(imc >= 30 && imc < 40) {
+        console.log("Obesidade");
+    } else {
+        console.log("Obesidade mórbida");
     }
 
-    var weightIdealMin = (18.5 * Math.pow(idHeight, 2));
-    var weightIdealMax = (25 * Math.pow(idHeight, 2));
-    console.log("peso ideal: \nmin: " + weightIdealMin.toFixed(3) + " kg\nmax: " + weightIdealMax.toFixed(3) + " kg");
+    var minIdealWeight = Math.pow(idHeight, 2) * 18.5;
+    var maxIdealWeight = Math.pow(idHeight, 2) * 24.9;
+
+    console.log("Peso ideal para sua altura:\nMin: " +
+                minIdealWeight.toFixed(3) + " kg\nMax: " +
+                maxIdealWeight.toFixed(3) + " kg");
 });

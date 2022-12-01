a38b55fec9987d37190b517914c7a624605bb1f7
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2022-12-01 18:04:12 -0300

    Delete script.js

--- range/js/script.js
+++
@@ -1,9 +0,0 @@
-var rangeTextBox = document.querySelector("#range-text-box");
-var textBox = document.querySelector("#text-box");
-var font;
-
-rangeTextBox.addEventListener("input", function(){
-    font = rangeTextBox.value / 5;
-    textBox.style.fontSize = font + "pt";
-    console.log(font);
-});
